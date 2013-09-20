3cb113032371f95a7df93a07e7009b0ac5c8c3da
j_ <alex.jann2012@gmail.com>
2013-09-20 12:15:22 -0500

    Progress push. all Test goes on testsql.py

--- testTrain.py
+++ testTrain.py
@@ -10,6 +10,12 @@ import httplib
 import json
 from random import randint
 
+#######################################################################################
+#
+# Global variables
+#
+#######################################################################################
+
 scene = getSceneManager()
 all = SceneNode.create("everything")
 
@@ -21,18 +27,48 @@ trainRoot = SceneNode.create("trainTracking")
 all.addChild(trainRoot)
 sceneLayer.update( {"trainTracking": trainRoot} )
 
-setNearFarZ(1, 2 * 23227.75)
-#deal with the camera
-cam = getDefaultCamera()
-cam.setPosition(Vector3(439418.67, 4631157.25, -752.96) + Vector3(7768.82, 2281.18, 2034.08))
-#cam.setPosition(city1.getBoundCenter() )
-cam.getController().setSpeed(2000)
-cam.pitch(3.14159*0.45) #pitch up to start off flying over the city
-#set up the scene
+updateTime = 20
 
-light = Light.create()
-light.setColor(Color('white'))
-light.setEnabled(True)
+showTrainFlag = True;
+showRouteFlag = True;
+showStationFlag = True;
+showMapFlag = 0;
+showCrimeFlag = True;
+CrimeSimulationFlag = False;
+
+#######################################################################################
+#
+# Callback Func
+#
+#######################################################################################
+
+def updateFunc(frame, time, dt):
+	global updateTime
+	if ( time > updateTime ):
+		updateTrain()
+		print "updateTrain"
+		updateTime = time + 20
+		
+#######################################################################################
+#
+# Set up the scene
+#
+# GUI shoul be put here too
+#
+#######################################################################################
+def initialize():
+	setNearFarZ(1, 2 * 23227.75)
+	#deal with the camera
+	cam = getDefaultCamera()
+	cam.setPosition(Vector3(439418.67, 4631157.25, -752.96) + Vector3(7768.82, 2281.18, 2034.08))
+	#cam.setPosition(city1.getBoundCenter() )
+	cam.getController().setSpeed(2000)
+	cam.pitch(3.14159*0.45) #pitch up to start off flying over the city
+	#set up the scene
+
+	light = Light.create()
+	light.setColor(Color('white'))
+	light.setEnabled(True)
 
 ########################################################################################
 #
@@ -42,71 +78,71 @@ light.setEnabled(True)
 # communityList 	== list of the name of each community
 #
 ########################################################################################
+def buildCommunity():
+	vector = ".\\data\\communities.kml"
 
-vector = ".\\data\\communities.kml"
+	xmldoc = minidom.parse(vector)
 
-xmldoc = minidom.parse(vector)
+	communityList = []
+	itemlist = xmldoc.getElementsByTagName('description')
+	for node in itemlist:
+		str = node.firstChild.data.encode('ascii', 'ignore')
+		list = str.split('\n')
+		for line in list:
+			if ( re.search('COMMUNITY', line) != None ):
+				xml = minidom.parseString(line)
+				tag = xml.getElementsByTagName('span')
+				communityList.append(tag[1].firstChild.data.encode('ascii', 'ignore'))
+				break
 
-communityList = []
-itemlist = xmldoc.getElementsByTagName('description')
-for node in itemlist:
-	str = node.firstChild.data.encode('ascii', 'ignore')
-	list = str.split('\n')
-	for line in list:
-		if ( re.search('COMMUNITY', line) != None ):
-			xml = minidom.parseString(line)
-			tag = xml.getElementsByTagName('span')
-			communityList.append(tag[1].firstChild.data.encode('ascii', 'ignore'))
-			break
-
-viewposition = []
-index = 0
-itemlist = xmldoc.getElementsByTagName('MultiGeometry') 
-for node in itemlist:
-	r = randint(0,255)
-	g = randint(0,255)
-	b = randint(0,255)
-	color = Color(r/255.0, g/255.0, b/255.0, 1)
-	coord = node.getElementsByTagName('coordinates')
-	first = True
-	for line in coord:
-		str = line.firstChild.nodeValue.encode('ascii', 'ignore')
-		if (first):
-			first = False
-			xy = str.partition(',')
-			result = utm.from_latlon(float(xy[2]), float(xy[0]))
-			viewposition.append(Vector3( float(result[0]),float(result[1]), 0 ) )
-			t = Text3D.create('fonts/arial.ttf', 200, communityList[index])
-			t.setPosition( viewposition[index] + Vector3(0,0,300))
-			t.setColor( color )
-			districtBound = LineSet.create()
-			districtBound.setEffect('colored -e #' + hex(r)[2:] + hex(g)[2:] + hex(b)[2:] + 'dd -t')
-			l = districtBound.addLine()
-			l.setStart(viewposition[index])
-			l.setEnd(viewposition[index] + Vector3(0,0,300))
-			l.setThickness(30.0)
-			#t.setFixedSize(True)
-			#t.setFacingCamera(cam)
-			index+=1
-		else:
-			firstTime = True
-			pointArray = str.split(' ')
-			for pos in pointArray:
-				xy = pos.partition(',')
+	viewposition = []
+	index = 0
+	itemlist = xmldoc.getElementsByTagName('MultiGeometry') 
+	for node in itemlist:
+		r = randint(0,255)
+		g = randint(0,255)
+		b = randint(0,255)
+		color = Color(r/255.0, g/255.0, b/255.0, 1)
+		coord = node.getElementsByTagName('coordinates')
+		first = True
+		for line in coord:
+			str = line.firstChild.nodeValue.encode('ascii', 'ignore')
+			if (first):
+				first = False
+				xy = str.partition(',')
 				result = utm.from_latlon(float(xy[2]), float(xy[0]))
-				if firstTime == True:
-					firstTime = False
-					oldX = float(result[0])
-					oldY = float(result[1])
-				elif ( abs(Vector2(oldX, oldY) - Vector2( float(result[0]),float(result[1]))) > 30 ):
-					l = districtBound.addLine()
-					l.setStart(Vector3(oldX, oldY, 10))
-					l.setEnd(Vector3(float(result[0]), float(result[1]), 10))
-					l.setThickness(40.0)
-					oldX = float(result[0])
-					oldY = float(result[1])
-			firstTime = True	
-	first = True
+				viewposition.append(Vector3( float(result[0]),float(result[1]), 0 ) )
+				t = Text3D.create('fonts/arial.ttf', 200, communityList[index])
+				t.setPosition( viewposition[index] + Vector3(0,0,300))
+				t.setColor( color )
+				districtBound = LineSet.create()
+				districtBound.setEffect('colored -e #' + hex(r)[2:] + hex(g)[2:] + hex(b)[2:] + 'dd -t')
+				l = districtBound.addLine()
+				l.setStart(viewposition[index])
+				l.setEnd(viewposition[index] + Vector3(0,0,300))
+				l.setThickness(30.0)
+				#t.setFixedSize(True)
+				#t.setFacingCamera(cam)
+				index+=1
+			else:
+				firstTime = True
+				pointArray = str.split(' ')
+				for pos in pointArray:
+					xy = pos.partition(',')
+					result = utm.from_latlon(float(xy[2]), float(xy[0]))
+					if firstTime == True:
+						firstTime = False
+						oldX = float(result[0])
+						oldY = float(result[1])
+					elif ( abs(Vector2(oldX, oldY) - Vector2( float(result[0]),float(result[1]))) > 30 ):
+						l = districtBound.addLine()
+						l.setStart(Vector3(oldX, oldY, 10))
+						l.setEnd(Vector3(float(result[0]), float(result[1]), 10))
+						l.setThickness(40.0)
+						oldX = float(result[0])
+						oldY = float(result[1])
+				firstTime = True	
+		first = True
 
 
 ########################################################################################
@@ -211,6 +247,24 @@ def moveTrain(json_str):
 		else:
 			train.setPosition(record['x'], record['y'], record['z'])
 			train.setVisible(True)
-getStation()
-getRoute()
-updateTrain()
+			
+			
+#######################################################################################
+#
+# Crime
+#
+#######################################################################################
+
+#######################################################################################
+#
+# Entry Point
+#
+#######################################################################################
+			
+if (__	name__ == "__main__"):
+	initialize()
+	buildCommunity()			
+	getStation()
+	getRoute()
+	updateTrain()
+	setUpdateFunction(updateFunc)
--- testsql.py
+++ testsql.py
@@ -1,21 +1,30 @@
 import sys
 sys.path.append('/home/evl/cs526/j_/libs')
 import mysql.connector
+from mysql.connector import errorcode
 #hostAdd = raw_input()
 
+if (isMaster()):
+	hostAdd = 'localhost'
+	try:
+		cnx = mysql.connector.connect(user='view', host = hostAdd, database='crime')
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Something is wrong with your user name or password")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database does not exists")
+		else:
+			print(err)
+	else: 	
+		cursor = cnx.cursor()
+		query = ("select -20")
+		cursor.execute(query)
+		for name in cursor:
+				
+				if (isMaster()):
+					broadcastCommand('''SphereShape.create(1,4).setPosition( Vector3(0, 0, ''' + str(name[0]) + '''));''')
+			
 
-hostAdd = '131.193.79.42'
-cnx = mysql.connector.connect(user='view', host = hostAdd, database='crime')
-
-cursor = cnx.cursor()
-query = ("select -20")
-cursor.execute(query)
-for name in cursor:
-		
-		if (isMaster()):
-			broadcastCommand('''SphereShape.create(1,4).setPosition( Vector3(0, 0, ''' + str(name[0]) + '''));''')
-		
-
-print "finished"
-cursor.close()
-#q = threading.Timer(3.0, target = update, args = (cnx,))
+		print "finished"
+		cursor.close()
+	#q = threading.Timer(3.0, target = update, args = (cnx,))
